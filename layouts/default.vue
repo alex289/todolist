fa3ccee8a79d2bb2fd5e20ca573ecf8aebfa4542
alex289 <mccr4fthd@gmail.com>
2023-07-30 13:55:10 +0200

    Rename package json name

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,28 +7,63 @@ const isUserMenuOpen = ref(false);
 
 <template>
   <div>
-    <nav class="bg-white border-gray-200 dark:bg-gray-900">
-      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <nav class="border-gray-200 bg-white dark:bg-gray-900">
+      <div
+        class="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4"
+      >
         <a href="/" class="flex items-center">
-          <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Todolist logo">
-          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Todolist</span>
+          <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            class="mr-3 h-8"
+            alt="Todolist logo"
+          >
+          <span
+            class="self-center whitespace-nowrap text-2xl font-semibold dark:text-white"
+          >Todolist</span>
         </a>
-        <div class="flex items-center md:order-2 relative">
-          <button v-if="status === 'authenticated'" type="button" class="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" @click="isUserMenuOpen = !isUserMenuOpen">
-            <img class="w-8 h-8 rounded-full" :src="data?.user?.image ?? 'https://flowbite.com/docs/images/logo.svg'" alt="user photo">
+        <div class="relative flex items-center md:order-2">
+          <button
+            v-if="status === 'authenticated'"
+            type="button"
+            class="mr-3 flex rounded-full bg-gray-800 text-sm focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600 md:mr-0"
+            @click="isUserMenuOpen = !isUserMenuOpen"
+          >
+            <img
+              class="h-8 w-8 rounded-full"
+              :src="
+                data?.user?.image ?? 'https://flowbite.com/docs/images/logo.svg'
+              "
+              alt="user photo"
+            >
           </button>
-          <button v-if="status === 'unauthenticated'" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" @click="signIn('github')">
+          <button
+            v-if="status === 'unauthenticated'"
+            type="button"
+            class="mb-2 mr-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            @click="signIn('github')"
+          >
             Sign in
           </button>
           <!-- Dropdown menu -->
-          <div v-if="isUserMenuOpen" class="z-50 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600 absolute top-full right-0">
+          <div
+            v-if="isUserMenuOpen"
+            class="absolute right-0 top-full z-50 my-4 list-none divide-y divide-gray-100 rounded-lg bg-white text-base shadow dark:divide-gray-600 dark:bg-gray-700"
+          >
             <div class="px-4 py-3">
-              <span class="block text-sm text-gray-900 dark:text-white">{{ data?.user?.name }}</span>
-              <span class="block text-sm  text-gray-500 truncate dark:text-gray-400">{{ data?.user?.email }}</span>
+              <span class="block text-sm text-gray-900 dark:text-white">{{
+                data?.user?.name
+              }}</span>
+              <span
+                class="block truncate text-sm text-gray-500 dark:text-gray-400"
+              >{{ data?.user?.email }}</span>
             </div>
             <ul class="py-2">
               <li>
-                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white" @click="signOut()">Sign out</a>
+                <a
+                  href="#"
+                  class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
+                  @click="signOut()"
+                >Sign out</a>
               </li>
             </ul>
           </div>
